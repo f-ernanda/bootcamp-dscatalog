cafd67a96ac9e5ebcd916aef9d00daa6f8ce0474
Fernanda <f_ernanda@outlook.com.br>
2021-11-19 10:03:12 -0300

    Add DTO

--- backend/src/main/java/dev/fernanda/dscatalog/services/CategoryService.java
+++ backend/src/main/java/dev/fernanda/dscatalog/services/CategoryService.java
@@ -1,5 +1,6 @@
 package dev.fernanda.dscatalog.services;
 
+import dev.fernanda.dscatalog.dto.CategoryDTO;
 import dev.fernanda.dscatalog.entities.Category;
 import dev.fernanda.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,7 +17,8 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream().map(CategoryDTO::new).collect(Collectors.toList());
     }
 }
